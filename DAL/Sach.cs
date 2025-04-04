a338b7cb47e0b72b823ff2fc660459924f3411d6
Hung Hao Han <lequanghung.work@gmail.com>
2025-04-04 11:26:08 +0700

    Update: Thêm 2 models DanhMucSach và Sách, cấu hình trước DanhMucSach

---
+++ DAL/Sach.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class Sach
+    {
+        public string MaSach { get; set; }
+        public string ISBN { get; set; }
+        public string TenSach { get; set; }
+        public string TacGia { get; set; }
+        public string MaDanhMuc { get; set; }
+        public string NhaXuatBan { get; set; }
+        public int NamXuatBan { get; set; }
+        public string NXB { get; set; }
+        public int SoLuong { get; set; }
+        public int KhaDung { get; set; }
+        public string ViTri { get; set; }
+
+        public virtual DanhMucSach DanhMucSanh { get; set; } 
+    }
+}
---
+++ DAL/SachContext.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace DAL
+{
+    public class SachContext : DbContext
+    {
+        public SachContext() : base("SachDbConnection") { }
+
+        public DbSet<DanhMucSach> DanhMucSachs { get; set; }
+        public DbSet<Sach> Sachs { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //Cấu hình danh mục sách
+            var danhMucSachConfig = modelBuilder.Entity<DanhMucSach>();
+            danhMucSachConfig.HasKey(dm => dm.MaDanhMuc); //Khóa chính
+            danhMucSachConfig.Property(dm => dm.MaDanhMuc)
+                .HasMaxLength(20); //Mã danh mục sách không được null và có độ dài tối đa 20 ký tự
+
+            danhMucSachConfig.Property(dm => dm.TenDanhMuc)
+                .IsRequired() //Tên danh mục sách không được null
+                .HasMaxLength(255); //Độ dài tối đa 255 ký tự
+
+            danhMucSachConfig.Property(dm => dm.MoTa)
+                .HasMaxLength(500); //Độ dài tối đa 500 ký tự
+
+            danhMucSachConfig.Property(dm => dm.DanhMucCha)
+                .HasMaxLength(20); //Mã danh mục cha có độ dài tối đa 20 ký tự
+
+            danhMucSachConfig.Property(dm => dm.SoLuongSach)
+                .IsRequired() // Số lượng sách không được null
+                .HasColumnAnnotation("Default", 0) // Giá trị mặc định là 0
+                .HasColumnAnnotation("Check", "SoLuongSach >= 0"); // Kiểm tra số lượng sách không âm
+
+            danhMucSachConfig.Property(dm => dm.NgayTao)
+                .IsRequired() // Ngày tạo không được null
+                .HasColumnAnnotation("Default", DateTime.Now); // Giá trị mặc định là ngày hiện tại
+
+            danhMucSachConfig.Property(dm => dm.CapNhatLanCuoi)
+                .IsRequired() // Ngày cập nhật không được null
+                .HasColumnAnnotation("Default", DateTime.Now); // Giá trị mặc định là ngày hiện tại
+
+            danhMucSachConfig.Property(dm => dm.TrangThai)
+                .IsRequired() // Trạng thái không được null
+                .HasMaxLength(20) // Độ dài tối đa 20 ký tự
+                .HasColumnAnnotation("Default", "Hoạt động") // Giá trị mặc định là "Active"
+                .HasColumnAnnotation("Check", "TrangThai IN ('Hoạt động', 'Ngừng hoạt động')"); // Kiểm tra trạng thái hợp lệ
+
+
+
+
+        }
+
+
+    }
+}
