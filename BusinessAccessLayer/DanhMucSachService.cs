77f6337ff373c6353ce732e9e2b3220e2ffdd54f
Hung Hao Han <lequanghung.work@gmail.com>
2025-04-04 16:35:12 +0700

    Update: Hoàn thiện cấu hình Sách và Danh mục sách, cấu hình hàm load thông tin và thêm cơ bản

---
+++ BusinessAccessLayer/DanhMucSachService.cs
@@ -0,0 +1,35 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace BusinessAccessLayer
+{
+    public class DanhMucSachService
+    {
+        private readonly SachContext _context;
+
+        public DanhMucSachService()
+        {
+            _context = new SachContext();
+        }
+
+
+        public List<DanhMucSach> LayTatCaDanhMucSach()
+        {
+            return _context.DanhMucSachs.ToList();
+        }
+
+        public void ThemDanhMucSach(DanhMucSach danhMucSachObj)
+        {
+            _context.DanhMucSachs.Add(danhMucSachObj);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ BusinessAccessLayer/SachService.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessAccessLayer
+{
+    public class SachService
+    {
+        private readonly SachContext _context;
+        public SachService()
+        {
+            _context = new SachContext();
+        }
+
+        public void ThemSach(Sach sach)
+        {
+            _context.Sachs.Add(sach);
+            _context.SaveChanges();
+        }
+    }
+}
